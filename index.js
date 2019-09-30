9b5e3f798816ce96214960e34246f4e2dfd56349
chenyinl <linchenyin@gmail.com>
2019-09-30 16:06:02 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
 const axios = require('axios');
 module.exports = {
-	function1: function(a,b,c){
-		console.log(a);
-		console.log(b);
-		console.log(c);
-	},
-	
-	retry3: function(
-	    method,
-		url,
-		payload
+    function1: function(a,b,c){
+        console.log(a);
+        console.log(b);
+        console.log(c);
+    },
+    
+    retry3: function(
+        method,
+        url,
+        payload
 
-	){
-		const res =  axios.get( url, {
+    ){
+        const res =  axios.get( url, {
             headers: {
                 'Content-Type': 'application/json'
             },
             timeout: 1000
             })
-            .then(function(response){
-		        //console.log(response); // too much data to log
-		        console.log("success");
-		    })
-		    .catch(function(error){
-		        //console.log(error.code);
-		        //console.log(error.message);
-		        if(error.message.includes('timeout')){
-					console.log('TIMEOUT happened');
-				}
-		        
-		    });
-	}
+        .then(function(response){
+                //console.log(response); // too much data to log
+                console.log("success");
+        })
+        .catch(function(error){
+            /* example of handle the error by http code */
+            if(error.response.status == 502){
+                console.log("The error is bad gataway.");
+            }
+            /* example of handle the error by error message */
+            if(error.message.includes('timeout')){
+                console.log('TIMEOUT happened');
+            }
+        });
+    }
 }
